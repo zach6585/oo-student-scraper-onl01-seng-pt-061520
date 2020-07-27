5cb84d60cfbaa1d92914922f15da4a695ca2f266
zachary mines <zmines@wesleyan.edu>
2020-07-27 21:19:34 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,7 +5,8 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    @student_hash = {}
+    @name = student_hash[:name]
+    
   end
 
   def self.create_from_collection(students_array)
