154a8e66dd6a84675aab3caeb0212026eb2984a5
zachary mines <zmines@wesleyan.edu>
2020-07-27 21:21:54 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -18,7 +18,8 @@ class Student
 
   def add_student_attributes(attributes_hash)
     attributes_hash.each do |key, value|
-      studen_hash
+      studen_hash[key] = value 
+    end 
   end
 
   def self.all
